c350906fc16d534c465c74a829e5e1ea92a8ea51
Morgen Sudyanto <moondemon68@gmail.com>
2019-08-08 21:06:38 +0700

    Add menu module

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const fs = require("fs");
+const multer = require("multer");
+const crypto = require("crypto");
+const mime = require("mime");
+var storage = multer.diskStorage({
+    destination: __dirname + '/public/images',
+    filename: function (req, file, cb) {
+      crypto.pseudoRandomBytes(16, function (err, raw) {
+        cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
+      });
+    }
+});
+var upload = multer({ storage: storage });
 
 const app = express();
 
@@ -14,29 +26,50 @@ app.use(express.static("public"));
 
 // VARIABLES
 var menu = {
-    imageFile: [],
-    pelapor: [],
-    laporan: []
+    imagename: [],
+    nama: []
 };
 // END OF VARIABLES
 
-// READ FILES
-/*
-fs.readFile('menu.json','utf8',function (err,data) {
-    if (err) {
-        console.log(err);
-    } else {
-        menu = JSON.parse(data);
-    }
-});
-*/
-// END OF READ FILES
+// READ AND WRITE FILES
+function readFileFunc() {
+    fs.readFile('menu.json','utf8',function (err,data) {
+        if (err) {
+            console.log(err);
+        } else {
+            menu = JSON.parse(data);
+        }
+    });
+}
+
+function writeFileFunc() {
+    var json = JSON.stringify(menu);
+    fs.writeFile('database/menu.json',json,'utf8',function(req,res) {
+        console.log(json);
+    });
+}
+
+// END OF READ AND WRITE FILES
 
 // HOME PAGE
 app.get("/",function(req,res) {
+    readFileFunc();
     res.render("index",{menu:menu});
 });
 // END OF HOME PAGE
+
+// ADD MENU
+app.get("/addmenu",function(req,res) {
+    res.render("addmenu");
+});
+app.post("/addmenu",upload.single('photo'), (req,res) => {
+    menu.imagename.push(req.file.filename);
+    menu.nama.push(req.body.nama);
+    console.log(req.file);
+    writeFileFunc();
+    res.redirect("/");
+});
+// END OF ADD MENU
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
