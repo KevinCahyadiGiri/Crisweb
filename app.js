6614e1d78ea08f04229fa5f2dce40e46e69cc799
Morgen Sudyanto <moondemon68@gmail.com>
2019-08-10 23:05:43 +0700

    Add listen port

--- app.js
+++ app.js
@@ -122,6 +122,6 @@ app.get("/wipealldtbs",function(req,res) {
 })
 // END OF WIPE DATABASE
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
